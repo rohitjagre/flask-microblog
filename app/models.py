a46a9e143521adc41776211236c3247c6d7f5043
Rohit Jagre <rohit.jagre@elucidata.io>
2020-03-30 00:10:05 +0530

    email support and password reset feature added

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-from app import db, login
 from hashlib import md5
+import jwt
+from app import db, login, app
+from time import time
 
 
 followers = db.Table('followers',
@@ -54,6 +56,19 @@ class User(UserMixin, db.Model):
                 followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=[
+                            'HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
