88dd1889cb939a7962d230083bebfb3001270df2
Rohit Jagre <rohit.jagre@elucidata.io>
2020-03-29 22:51:50 +0530

    pagination done

--- config.py
+++ config.py
@@ -15,4 +15,6 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['rohit.jagre@elucidata.io']
+    ADMINS = ['your@email.com']
+    # Application specific
+    POSTS_PER_PAGE = 10
