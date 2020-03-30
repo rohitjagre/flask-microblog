7c337699e13545b24a4389a24bdcc7fdc60adc00
Rohit Jagre <rohit.jagre@elucidata.io>
2020-03-30 16:51:54 +0530

    added date and time

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from logging.handlers import RotatingFileHandler, SMTPHandler
 import logging
 import os
@@ -17,6 +18,7 @@ migrate = Migrate(app=app, db=db)
 login = LoginManager(app)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 login.login_view = 'login'
 
